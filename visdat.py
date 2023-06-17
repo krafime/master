7c151b0fb10b029cf2b5d1df195211fc1bacf305
krafime <krafime@student.telkomuniversity.ac.id>
2023-06-17 16:03:26 +0700

    change version

--- visdat.py
+++ visdat.py
@@ -1,57 +1,63 @@
 import calendar
-
 import pandas as pd
-from bokeh.layouts import row
+import streamlit as st
 from bokeh.models import ColumnDataSource, Select, Slider
 from bokeh.palettes import Category20
-from bokeh.plotting import curdoc, figure
+from bokeh.plotting import figure
 
-# Membaca data dari file CSV
-data = pd.read_csv('austin_weather.csv')
-data['Date'] = pd.to_datetime(data['Date'])
 
-# Mengurutkan bulan berdasarkan urutan kalender
-months = list(calendar.month_name)[1:]
+def load_data():
+    # Membaca data dari file CSV
+    data = pd.read_csv('austin_weather.csv')
+    data['Date'] = pd.to_datetime(data['Date'])
+    return data
 
-# Membuat pilihan unik untuk tahun dan bulan
-years = sorted(data['Date'].dt.year.unique().tolist())
-months = sorted(months, key=lambda x: list(calendar.month_name).index(x))
 
-# Membuat data source Bokeh dari DataFrame
-source = ColumnDataSource(data)
+def filter_data(data, selected_year, selected_month):
+    filtered_data = data[
+        (data['Date'].dt.year == selected_year) & (data['Date'].dt.month_name() == selected_month)
+    ]
+    return filtered_data
 
-# Membuat plot
-plot = figure(x_axis_type="datetime", title="Grafik Seluruh Suhu Rata-rata Harian dalam satuan Fahrenheit di kota Austin dari Tahun 2013-2017", width=1200, height=600)
-plot.line('Date', 'TempAvgF', source=source, line_width=2)
 
-# Membuat slider untuk tahun
-year_slider = Slider(title="Tahun", start=min(years), end=max(years), value=min(years), step=1)
+def main():
+    # Load data
+    data = load_data()
 
-# Membuat dropdown menu untuk bulan
-month_select = Select(title="Bulan", options=months, value=months[0])
+    # Mengurutkan bulan berdasarkan urutan kalender
+    months = list(calendar.month_name)[1:]
 
-# Fungsi untuk memperbarui data yang ditampilkan berdasarkan slider dan dropdown menu
-def update_data(attrname, old, new):
-    selected_year = year_slider.value
-    selected_month = month_select.value
+    # Membuat pilihan unik untuk tahun dan bulan
+    years = sorted(data['Date'].dt.year.unique().tolist())
+    months = sorted(months, key=lambda x: list(calendar.month_name).index(x))
 
-    filtered_data = data[(data['Date'].dt.year == selected_year) & (data['Date'].dt.month_name() == selected_month)]
+    # Membuat plot
+    plot = figure(x_axis_type="datetime", title="Grafik Seluruh Suhu Rata-rata Harian dalam satuan Fahrenheit di kota Austin dari Tahun 2013-2017", width=1200, height=600)
+
+    # Menampilkan plot kosong
+    plot.line([], [], line_width=2)
+
+    # Membuat slider untuk tahun
+    selected_year = st.sidebar.slider("Tahun", min_value=min(years), max_value=max(years), value=min(years), step=1)
+
+    # Membuat dropdown menu untuk bulan
+    selected_month = st.sidebar.selectbox("Bulan", months, index=0)
+
+    # Fungsi untuk memperbarui data yang ditampilkan berdasarkan slider dan dropdown menu
+    filtered_data = filter_data(data, selected_year, selected_month)
 
     if filtered_data.empty:
         # Jika data kosong, menampilkan tulisan "Data tidak tersedia"
         plot.title.text = "Data tidak tersedia"
-        source.data = dict()
     else:
         # Jika data tersedia, menampilkan data yang difilter
+        source = ColumnDataSource(filtered_data)
+        plot.line('Date', 'TempAvgF', source=source, line_width=2)
         plot.title.text = "Grafik Suhu Rata-rata Harian dalam satuan Fahrenheit di kota Austin"
-        source.data = dict(ColumnDataSource(filtered_data).data)
+
+    # Menampilkan plot menggunakan Streamlit
+    st.bokeh_chart(plot)
 
 
-# Mengikat fungsi update_data dengan perubahan pada slider dan dropdown menu
-year_slider.on_change('value', update_data)
-month_select.on_change('value', update_data)
-
-# Menambahkan elemen ke layout
-layout = row(year_slider, month_select)
-curdoc().add_root(layout)
-curdoc().add_root(plot)
+if __name__ == "__main__":
+    main()
